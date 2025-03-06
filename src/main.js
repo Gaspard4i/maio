0eea5f4c3268f69ba945b4137b3c6a1af7853135
gabriel mille <gabriel.mille.etu@univ-lille.fr>
2025-03-06 17:19:16 +0100

    Implement canvas resizing and update drawing logic

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
 const canvas = document.querySelector('.gameCanvas');
 const context = canvas.getContext('2d');
+let x = 0;
+let y = 0;
+let vx = 0;
+let vy = 0;
 
-context.moveTo(0, 0);
-context.beginPath();
-context.arc(
-	canvas.getAttribute('width') / 2,
-	canvas.getAttribute('height') / 2,
-	10,
-	0,
-	2 * Math.PI
-);
-context.fill();
-context.stroke();
+const canvasResizeObserver = new ResizeObserver(() => resampleCanvas());
+canvasResizeObserver.observe(canvas);
+
+function resampleCanvas() {
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
+    draw();
+}
+
+function draw() {
+    context.moveTo(0, 0);
+    context.beginPath();
+    context.arc(
+        canvas.width / 2,
+        canvas.height / 2,
+        30,
+        0,
+        2 * Math.PI
+    );
+    context.fill();
+    context.stroke();
+}
