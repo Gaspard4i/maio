631d8769ef94ad317a66d984d724c46c0603559e
Gaspard4i <gaspard.catry.etu@univ-lille.fr>
2025-03-11 12:04:16 +0100

    refactor entity

--- src/entity.js
+++ src/entity.js
@@ -1,13 +1,11 @@
 export class Entity {
-	constructor(radius, x, y, vx, vy) {
+	constructor(radius, x, y) {
 		if (this.constructor === Entity) {
 			throw new Error("Impossible d'instancier la classe abstraite Entity");
 		}
 		this.radius = radius;
 		this.x = x;
 		this.y = y;
-		this.vx = vx;
-		this.vy = vy;
 	}
 
 	draw(context) {
