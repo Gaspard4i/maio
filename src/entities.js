3b1d8452f82555c38dc06bbfb7b2889e680cf38f
gabriel mille <gabriel.mille.etu@univ-lille.fr>
2025-03-11 12:01:56 +0100

    generation des premieres taches aleatoirement

--- src/entities.js
+++ src/entities.js
@@ -1,15 +1,9 @@
 import { Stain } from './stain.js';
+import { canvas } from './canvas.js';
 
-export const stains = [new Stain(20, 200, 200, 0, 0)];
-
-for (let i = 0; i < 30; i++) {
-	stains.push(
-		new Stain(
-			20 + i, // Modifier la taille
-			200 - i * 5, // Modifier la position x
-			200 - i * 5, // Modifier la position y
-			i * 10, // Modifier la rotation
-			i * 10 // Modifier l'opacité
-		)
-	);
+export const stains = [];
+for (let i = 0; i < 20; i++) {
+	const x = Math.floor(Math.random() * canvas.clientWidth-10);
+	const y = Math.floor(Math.random() * canvas.clientHeight-10);
+	stains.push(new Stain(20, x, y, 0, 0));
 }
