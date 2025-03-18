dbbb2ea7204b20d7b1492c91496cb6f1b50e279d
gabriel mille <gabriel.mille.etu@univ-lille.fr>
2025-03-18 14:30:53 +0100

    test player applyAcceleration, updateVelocity et applyFriction

--- test/player.test.js
+++ test/player.test.js
@@ -33,4 +33,53 @@ describe('Player Module', () => {
 			assert.equal(player.radius, 15);
 		});
 	});
+
+	describe('applyAcceleration', () => {
+		it('should reduce radius when accelerating', () => {
+			const player = new Player(30, 0, 0, 0, 0, true);
+			player.keys['Shift'] = true;
+			player.applyAcceleration();
+			assert.equal(player.isAccelerating, true);
+			assert(player.radius < 30);
+		});
+	});
+
+	describe('applyFriction', () => {
+		it('should reduce velocity when sliding', () => {
+			const player = new Player(30, 0, 0, 10, 10, true);
+			player.isSliding = true;
+			player.applyFriction();
+			assert(player.vx < 10);
+			assert(player.vy < 10);
+		});
+
+		it('should stop sliding when velocity is near zero', () => {
+			const player = new Player(30, 0, 0, 0.05, 0.05, true);
+			player.isSliding = true;
+			player.applyFriction();
+			assert.equal(player.vx, 0);
+			assert.equal(player.vy, 0);
+			assert.equal(player.isSliding, false);
+		});
+	});
+
+	describe('updateVelocity', () => {
+		it('should update velocity based on keys pressed', () => {
+			const player = new Player(30, 0, 0, 0, 0, true);
+			player.keys['ArrowRight'] = true;
+			player.updateVelocity();
+			assert(player.vx > 0);
+			assert.equal(player.vy, 0);
+		});
+
+		it('should use accelerated speed when Shift is pressed', () => {
+			const player = new Player(30, 0, 0, 0, 0, true);
+			player.keys['Shift'] = true;
+			player.keys['ArrowUp'] = true;
+			player.updateVelocity();
+			assert(player.vy < 0);
+			assert.equal(player.vx, 0);
+			assert(player.isAccelerating);
+		});
+	});
 });
