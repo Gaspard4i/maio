655a8fcb7ad2e520979be0b3915f0c3f2cfa8ccd
gabriel mille <gabriel.mille.etu@univ-lille.fr>
2025-03-21 15:26:44 +0100

    remise en place du code gestion souris car suppr dans merge (calcul deplacement souris incorrects)

--- server/index.js
+++ server/index.js
@@ -51,9 +51,15 @@ export const processGameTick = () => {
 			};
 			player.updateVelocity(); // Met à jour la vitesse en fonction des touches
 			//TODO version clavier
-			player.movePlayer(stains); // Déplace le joueur
 		}
 	}
+
+	// Déplace tous les joueurs
+	for (const player of Object.values(players)) {
+		player.movePlayer(stains); // Déplace le joueur
+	}
+
+
 	// Met à jour les bots et les taches
 	bots.updateBots();
 	stains.updateStains();
@@ -64,6 +70,20 @@ export const processGameTick = () => {
 	io.emit('updateStains', stains);
 };
 
+function handleMouseMovement(socketId, x, y, canvaWidth, canvaHeight) {
+	const player = players[socketId];
+	const dx = x - (player.x % canvaWidth);
+	const dy = y - (player.y % canvaHeight);
+	player.updateMouseMovement(dx, dy, canvaWidth, canvaHeight);
+}
+
+function handleMouseMovementAcceleration(socketId, bool) {
+	const player = players[socketId];
+	player.isAccelerating = bool;
+
+}
+
+
 io.on('connection', socket => {
 	console.log(`Nouvelle connexion du client ${socket.id}`);
 
@@ -71,6 +91,14 @@ io.on('connection', socket => {
 
 	socket.on('input', bitmask => {
 		handleInput(socket.id, bitmask);
+	});
+
+	socket.on('mousemove', ({ x, y, canvaWidth, canvaHeight }) => {
+		handleMouseMovement(socket.id, x, y, canvaWidth, canvaHeight);
+	});
+
+	socket.on('mousedown', ({ bool }) => {
+		handleMouseMovementAcceleration(socket.id, bool);
 	});
 
 	socket.on('disconnect', () => {
